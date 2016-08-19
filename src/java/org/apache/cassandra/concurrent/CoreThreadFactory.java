56106e4f8f6431af219e796abac7eb67be4eb9bc
Bedri Sendir <bsendir1@binghamton.edu>
2016-08-19 12:43:59 -0500

    missing variables

--- src/java/org/apache/cassandra/concurrent/CoreThreadFactory.java
+++ src/java/org/apache/cassandra/concurrent/CoreThreadFactory.java
@@ -9,6 +9,9 @@ import org.apache.cassandra.config.Config;
 import io.netty.util.concurrent.FastThreadLocalThread;
 
 public class CoreThreadFactory implements ThreadFactory {
+	private int coreID = 0;
+	private int id = 0;
+
 	public CoreThreadFactory(int executorid) {
 		this.coreID = executorid;
 	}
@@ -17,8 +20,8 @@ public class CoreThreadFactory implements ThreadFactory {
 	public synchronized Thread newThread(Runnable r) {
 		Thread thread = new FastThreadLocalThread(new Runnable() {
 			@Override
-			public void run() {	
-			    Scheduler.instance.pinMe("others");
+			public void run() {
+				Scheduler.instance.pinMe("others");
 				r.run();
 			}
 		}, "Eventloop-" + coreID + "_" + id);
