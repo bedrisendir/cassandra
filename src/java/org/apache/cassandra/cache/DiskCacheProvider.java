4bfe9be1d1dcbeb23d38735e64064fb9ce63a8f8
Bedri Sendir <bsendir1@binghamton.edu>
2017-03-03 16:55:46 -0500

    update CAPI-Rowcache to current RowCache implementation/libraries
    —> replaced ConcurrentLinkedHashMap with Caffeine Cache
    —> ported changes for RowCacheKey and TableID

--- src/java/org/apache/cassandra/cache/DiskCacheProvider.java
+++ src/java/org/apache/cassandra/cache/DiskCacheProvider.java
@@ -7,6 +7,7 @@ import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
@@ -18,12 +19,14 @@ import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.io.ISerializer;
 import org.apache.cassandra.io.util.DataInputBuffer;
 import org.apache.cassandra.io.util.DataOutputBufferFixed;
+import org.apache.cassandra.schema.TableId;
 import org.apache.cassandra.utils.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
-import com.googlecode.concurrentlinkedhashmap.Weigher;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Weigher;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLong;
@@ -126,7 +129,7 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
 
     public static class FileRowCache implements ICache<RowCacheKey, IRowCacheEntry> {
 
-        private final ConcurrentLinkedHashMap<RowCacheKey, IRowCacheEntry> map;
+        private final Cache<RowCacheKey, IRowCacheEntry> map;
         private final ISerializer<IRowCacheEntry> serializer = new RowCacheSerializer();
         final HashFunction hashFunc;
         final AtomicInteger size = new AtomicInteger();
@@ -138,16 +141,17 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
         final long sizeInBytes;
 
         FileRowCache(long capacity) {
-            this.map = new ConcurrentLinkedHashMap.Builder<RowCacheKey, IRowCacheEntry>().weigher(new Weigher<IRowCacheEntry>() {
-                public int weightOf(IRowCacheEntry value) {
-                    long serializedSize = serializer.serializedSize(value);
-                    if (serializedSize > Integer.MAX_VALUE)
-                        throw new IllegalArgumentException("Unable to allocate " + serializedSize + " bytes");
+            this.map = Caffeine.newBuilder().weigher(new Weigher<RowCacheKey, IRowCacheEntry>(){
+				@Override
+				public int weigh(RowCacheKey key, IRowCacheEntry value) {
+					 long serializedSize = serializer.serializedSize(value);
+	                    if (serializedSize > Integer.MAX_VALUE)
+	                        throw new IllegalArgumentException("Unable to allocate " + serializedSize + " bytes");
 
-                    return (int) serializedSize;
-                }
-            }).maximumWeightedCapacity(DEFAULT_L2CACHE).concurrencyLevel(DEFAULT_CONCURENCY_LEVEL).build();
-
+	                    return (int) serializedSize;
+				}
+            }).maximumWeight(DEFAULT_L2CACHE).build();
+            
             String hashClass = System.getProperty("capi.hash");
             try {
                 hashFunc = hashClass == null ? new HashFunction() {
@@ -220,15 +224,16 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
         }
 
         public int hashCode(RowCacheKey key) {
-            int result = key.ksAndCFName.hashCode();
+            int result = key.tableId.hashCode();
             result = 31 * result + (key != null ? hashFunc.hashCode(key.key) : 0);
             return result;
         }
 
         public boolean equals(RowCacheKey key, ByteBuffer keyBB) {
-            if (!readString(keyBB).equals(key.ksAndCFName.left))
+        	TableId tableId = TableId.fromUUID(new UUID(keyBB.getLong(), keyBB.getLong()));
+            if (!tableId.equals(key.tableId))
                 return false;
-            if (!readString(keyBB).equals(key.ksAndCFName.right))
+            if (!readString(keyBB).equals(key.indexName))
                 return false;
             if (keyBB.remaining() != key.key.length)
                 return false;
@@ -242,11 +247,12 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
         public void serializeKey(RowCacheKey key, ByteBuffer bb) {
             try {
                 // logger.info("serializeKey=" + bb + ": " + key);
-                writeString(bb, key.ksAndCFName.left);
-                writeString(bb, key.ksAndCFName.right);
+                bb.putLong(key.tableId.asUUID().getMostSignificantBits());
+                bb.putLong(key.tableId.asUUID().getLeastSignificantBits());
+                writeString(bb, key.indexName);
                 bb.put(key.key);
             } catch (BufferOverflowException ex) {
-                logger.error("ideal: " + (getByteSizeForString(key.ksAndCFName.left) + getByteSizeForString(key.ksAndCFName.right) + key.key.length) + ", actual=" + bb.capacity());
+                logger.error("ideal: " + (key.tableId.serializedSize() + getByteSizeForString(key.indexName) + key.key.length) + ", actual=" + bb.capacity());
                 throw ex;
             }
         }
@@ -278,11 +284,12 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
         }
 
         public RowCacheKey deserializeKey(ByteBuffer bb) {
-            String ksName = readString(bb);
-            String cfName = readString(bb);
+            TableId tableId = TableId.fromUUID(new UUID(bb.getLong(), bb.getLong()));
+            String indexName = readString(bb);
+           
             ByteBuffer keyBody = ByteBuffer.allocateDirect(bb.remaining());
             keyBody.put(bb);
-            return new RowCacheKey(Pair.create(ksName, cfName), keyBody);
+            return new RowCacheKey(tableId, indexName, keyBody);
         }
 
         public IRowCacheEntry deserializeValue(ByteBuffer bb) {
@@ -295,7 +302,7 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
         }
 
         public int keySize(RowCacheKey k) {
-            return getByteSizeForString(k.ksAndCFName.left) + getByteSizeForString(k.ksAndCFName.right) + k.key.length;
+        	 return k.tableId.serializedSize() + getByteSizeForString(k.indexName) + k.key.length;
         }
 
         public int valueSize(IRowCacheEntry v) {
@@ -402,7 +409,7 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
 
         @Override
         public long weightedSize() {
-            return map.weightedSize();
+            return map.estimatedSize();
         }
 
         @Override
@@ -485,7 +492,7 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
                 if (!exist(hash, false))
                     return false;
 
-                IRowCacheEntry current = map.get(key);
+                IRowCacheEntry current = map.getIfPresent(key);
                 if (current == null) {
                     current = getFromFile(key, hash);
                     if (current == null) {
@@ -548,7 +555,7 @@ public class DiskCacheProvider implements org.apache.cassandra.cache.CacheProvid
 
             int hash = hashFunc.hashCode(key.key);
 
-            IRowCacheEntry entry = map.get(key);
+            IRowCacheEntry entry = map.getIfPresent(key);
             if (entry == null) {
                 if (exist(hash, false)) {
                     entry = getFromFile(key, hash);
